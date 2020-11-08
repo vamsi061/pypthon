7692ef0ba4eb0b124ce9550d9033fe51bb51a9ad
Vamsi <73982062+vamsi061@users.noreply.github.com>
2020-11-08 07:23:53 +0530

    files

---
+++ Day 2/sample.py
@@ -0,0 +1,14 @@
+print("hello everyone")
+print("waelcome to python")
+print("sum=",44+77)
+num1=10
+num2=20
+print("before swapping",num1,num2)
+#temp=num2
+#num2-num1
+#num1=temp
+'''num1=num1+num2
+num2=num1-num2
+num1=num1-num2'''
+num1,num2=num2,num1
+print("after swapping",num1,num2)
